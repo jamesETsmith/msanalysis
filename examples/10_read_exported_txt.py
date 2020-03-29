cbc1c71b0aff017c2de80382b056eb99001c682b
jamesETsmith <james.smith9113@gmail.com>
2020-03-29 08:38:38 -0600

    Adding reading mzXML example.

--- examples/10_read_exported_txt.py
+++ examples/10_read_exported_txt.py
@@ -13,11 +13,13 @@ from msanalysis.data_extraction import read_exported_txt
 from msanalysis.plotting.contour import contourf
 from msanalysis.data_processing.smoothing import moving_average
 
+# Set this path to
+mz, intensities = read_exported_txt("/home/james/Downloads/1-2260 (all).txt")
+# mz_lb, mz_ub = (60, 70)
+# mz_lb, mz_ub = (250, 280)
+mz_lb, mz_ub = (60, 280)
 
-mz, intensities = read_exported_txt("/home/james/Downloads/TiCl4_SnF4.npz")
-mz_lb, mz_ub = (60, 70)
-
-keep_ith_scan = 10
+keep_ith_scan = 1
 X, Y, Z = contourf(mz, intensities, mz_lb, mz_ub, keep_ith_scan=keep_ith_scan)
 
 #
@@ -34,8 +36,10 @@ plt.title("Regular Scale and No Smoothing")
 
 
 # Plot with log scale and NO smoothing
+log_min = Z.min() * 50
+
 plt.subplot(3, 1, 2)
-plt.contourf(X * keep_ith_scan, Y, Z, norm=colors.LogNorm(vmin=Z.min(), vmax=Z.max()))
+plt.contourf(X * keep_ith_scan, Y, Z, norm=colors.LogNorm(vmin=log_min, vmax=Z.max()))
 cbar = plt.colorbar()
 cbar.ax.set_ylabel("Log(Intensity)", rotation=270, fontsize=12)
 plt.title("Log Scale and NO Smoothing")
@@ -44,9 +48,9 @@ plt.title("Log Scale and NO Smoothing")
 # Plot with log scale AND smoothing
 plt.subplot(3, 1, 3)
 X, Y, Z = contourf(
-    mz, moving_average(intensities, n=150), mz_lb, mz_ub, keep_ith_scan=keep_ith_scan
+    mz, moving_average(intensities, n=5), mz_lb, mz_ub, keep_ith_scan=keep_ith_scan
 )
-plt.contourf(X * keep_ith_scan, Y, Z, norm=colors.LogNorm(vmin=Z.min(), vmax=Z.max()))
+plt.contourf(X * keep_ith_scan, Y, Z, norm=colors.LogNorm(vmin=log_min, vmax=Z.max()))
 cbar = plt.colorbar()
 cbar.ax.set_ylabel("Log(Intensity)", rotation=270, fontsize=12)
 plt.title("Log Scaling and Smoothing")
---
+++ examples/11_read_mzXML.py
@@ -0,0 +1,49 @@
+"""
+This example shows how to read an mzXML file exported by the Merlin software
+and make a contourf plots.
+
+Author: James E. T. Smith <james.smith9113@gmail.com>
+Date: 3/29/2020
+"""
+from pyopenms import MSExperiment, MzXMLFile
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from msanalysis.plotting.contour import contourf
+
+# Use PyOpenMS to read mzXML
+exp = MSExperiment()
+t0 = time.time()
+MzXMLFile().load("/home/james/Downloads/20200310_70.mzXML", exp)
+print(f"Time to load file {time.time()-t0}")
+
+# Dump List of PyOpenMS::Spectra to one NumPy array for MZ and a 2D array for
+# intensities
+n = len(exp.getSpectra())
+intensities = np.zeros((n, 12097))
+mz = exp.getSpectra()[0].get_peaks()[0]
+
+for i, s in enumerate(exp.getSpectra()):
+    intensities[i] = s.get_peaks()[1]
+
+# Select a subset of MZ range and plot intensities as a contour plot
+mz_lb, mz_ub = (60, 280)
+keep_ith_scan = 1
+X, Y, Z = contourf(mz, intensities, mz_lb, mz_ub, keep_ith_scan=keep_ith_scan)
+
+#
+# Plot
+#
+plt.figure()
+plt.plot(exp.getSpectra()[5000].get_peaks()[0], exp.getSpectra()[5000].get_peaks()[1])
+plt.xlim((190, 220))
+plt.ylabel("Intensitiy (UNITS UNKNOWN)")
+plt.xlabel("MZ")
+plt.savefig("single_spectra.png")
+
+plt.figure()
+plt.contourf(X * keep_ith_scan, Y, Z)
+cbar = plt.colorbar()
+cbar.ax.set_ylabel("Intensity (UNITS UNKNOWN)", rotation=270, fontsize=12, labelpad=15)
+plt.title("Regular Scale and No Smoothing")
+plt.savefig("test.png")
